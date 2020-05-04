9f824839b40a92cdebd6238d6c5d570731c0c818
Eric Sheppard <eric@eigenmo.de>
2020-05-04 12:29:39 +1000

    v0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aemo-EIGENMODE",
-    version="0.0.1",
+    version="0.0.2",
     author="Eric Sheppard",
     author_email="eric@eigenmo.de",
     description="A package to parse and verify AEMO MMS data model files",
